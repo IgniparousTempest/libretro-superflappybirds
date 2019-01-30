17bd41e0cd3667502e95aac2d4d71f10d1278615
IgniparousTempest <cr.pitcher@gmail.com>
2019-01-30 13:28:21 +0200

    Rotation

--- engine/renderer.hpp
+++ engine/renderer.hpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include "rect.hpp"
 #include "omp.h"
+#include "texture.hpp"
 
 class Renderer {
 public:
@@ -22,6 +23,8 @@ public:
         return r << 16 | g << 8 | b;
     }
 
+    /// Fill the screen with the specified colour.
+    /// \param colour The colour to fill the screen with.
     void Clear(uint32_t colour) {
 #pragma omp parallel for
         for (int x = 0; x <width; ++x) {
@@ -31,56 +34,23 @@ public:
         }
     }
 
-    void Render(Texture* image, Rect* dest) {
-#pragma omp parallel for
-        for (int x = 0; x < dest->w; ++x) {
-            int screen_x;
-            int screen_y;
-            uint32_t pixel;
-            int alpha;
-            for (int y = 0; y < dest->h; ++y) {
-                screen_x = dest->x + x;
-                screen_y = dest->y + y;
-                if (screen_x >= 0 && screen_y >= 0 && screen_x < width && screen_y < height) {
-                    pixel = image->image[y * dest->w + x];
-                    alpha = pixel >> 24;
-                    //TODO: This can only handle full alpha or no alpha
-                    if (alpha != 0)
-                        framebuffer[screen_y * width + screen_x] = pixel;
-                }
-            }
-        }
-    }
+    /// Renders the whole image of to the screen at the position and size defined in the rect.
+    /// \param image The image to draw to the screen.
+    /// \param dest The position and size to draw the image at.
+    void Render(Texture* image, Rect* dest);
 
+    /// Draws a part of an image to the screen.
+    /// \param image The image to draw to the screen.
+    /// \param src The subsection of the image to draw.
+    /// \param dest The position and size to draw the image at.
     void Render(Texture* image, Rect* src, Rect* dest);
 
-    void Render(Texture* image, Rect* src, Rect* dest, double angle) {
-        assert(src->w == dest->w && src->h == dest->h);
-        int hw = dest->w / 2;
-        int hh = dest->h / 2;
-
-#pragma omp parallel for
-        for (int x = -hw; x < hw * 3; ++x) {
-            int sx, sy, dest_index, alpha;
-            double s, c, px, py;
-            uint32_t pixel;
-            for (int y = -hh; y < hh * 3; ++y) {
-                s = std::sin(-angle * M_PI / 180);
-                c = std::cos(-angle * M_PI / 180);
-                px = x - hw;
-                py = y - hh;
-                // TODO: This is nearest-neighbour interpolation, use cubic instead. Once I support transparency better.
-                sx = std::round(px * c - py * s + hw);
-                sy = std::round(px * s + py * c + hh);
-                dest_index = (dest->y + y) * width + (dest->x + x);
-                pixel = image->image[(src->y + sy) * image->w + (src->x + sx)];
-                alpha = pixel >> 24;
-                //TODO: This can only handle full alpha or no alpha
-                if (alpha != 0 && sx >= 0 && sy >= 0 && sx < src->w && sy < src->h)
-                    framebuffer[dest_index] = pixel;
-            }
-        }
-    }
+    /// Draws an image to the screen.
+    /// \param image The image to draw to the screen.
+    /// \param src The subsection of the image to draw.
+    /// \param dest The position and size to draw the image at.
+    /// \param angle The angle in degrees to rotate the image.
+    void Render(Texture* image, Rect* src, Rect* dest, double angle);
 };
 
 
